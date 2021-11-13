7f76930debc27b11ba6cbb4affe71d170cfa56f5
Anonymous User <52798256+plocket@users.noreply.github.com>
2021-11-13 11:46:28 -0500

    Update styles to work with boostrap5

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
 setup(name='docassemble.multiselecttest',
       version='0.0.1',
       description=('A docassemble extension.'),
-      long_description='# docassemble.multiselecttest\n\nA docassemble extension.\n\n## Author\n\nSystem Administrator, admin@lemmalegal.com\n\n',
+      long_description='# docassemble.multiselecttest\r\n\r\nA docassemble extension.\r\n\r\n## Author\r\n\r\nSystem Administrator, admin@lemmalegal.com\r\n\r\n',
       long_description_content_type='text/markdown',
       author='System Administrator',
       author_email='admin@lemmalegal.com',
